9983a95015145ed032cbef395c4a1f2bd0f3765a
yceffort <yc.effort@navercorp.com>
2024-12-04 14:38:55 +0900

    [#69] baseIteratee 가 느려서 커스텀 구현

--- src/find.ts
+++ src/find.ts
@@ -1,17 +1,60 @@
-import {baseIteratee, ListIterateeCustom} from './internal/baseIteratee'
 import isArray from './isArray'
+import isEqual from './isEqual'
+import isObject from './isObject'
 
-export function find<T>(
-    collection: T[] | Record<string, T>,
-    predicate?: ListIterateeCustom<T, unknown>,
-): T | undefined {
+type Predicate<T> = (item: T, index: number, array: T[]) => boolean
+type Shorthand<T> = DeepPartial<T> | [keyof T, unknown] | keyof T
+
+type DeepPartial<T> = {
+    [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
+}
+
+function isPartialMatch<T>(item: T, partial: DeepPartial<T>): boolean {
+    if (!item || !partial) {
+        return false
+    }
+
+    return Object.keys(partial).every((key) => {
+        const partialValue = partial[key as keyof T]
+        const itemValue = item[key as keyof T]
+
+        if (isObject(partialValue) && isObject(itemValue)) {
+            return isPartialMatch(itemValue, partialValue as DeepPartial<typeof itemValue>)
+        }
+
+        return isEqual(itemValue, partialValue)
+    })
+}
+
+function isMatchPredicate<T>(item: T | undefined, predicate: Predicate<T> | Shorthand<T>): boolean {
+    if (!item) {
+        return false
+    }
+
+    if (typeof predicate === 'function') {
+        return predicate(item, 0, [])
+    }
+
+    if (isArray(predicate)) {
+        const [key, value] = predicate
+        return isEqual(item[key], value)
+    }
+
+    if (typeof predicate === 'string') {
+        return Boolean(item[predicate as keyof T])
+    }
+
+    return isPartialMatch(item, predicate as DeepPartial<T>)
+}
+
+export function find<T>(collection: T[] | Record<string, T>, predicate?: Predicate<T> | Shorthand<T>): T | undefined {
     const array = isArray(collection) ? collection : Object.values(collection)
 
-    const iteratee = baseIteratee(predicate)
+    const isMatch = predicate ? (item: T) => isMatchPredicate(item, predicate) : (item: T) => Boolean(item)
 
     for (let i = 0; i < array.length; i++) {
         const item = array[i]
-        if (item !== undefined && iteratee(item, i, array)) {
+        if (item !== undefined && isMatch(item)) {
             return item
         }
     }
