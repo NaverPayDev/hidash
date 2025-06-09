7923fa00ad2afedab8400a5895cbf77110dc0848
yceffort <yc.effort@navercorp.com>
2025-06-09 14:21:39 +0900

    📝 docs: enhance documentation for eq function with detailed parameter descriptions

--- src/eq.ts
+++ src/eq.ts
@@ -1,5 +1,10 @@
 /**
- * @see https://unpkg.com/lodash@4.17.21/eq.js
+ * @description Performs a SameValueZero comparison between two values to determine if they are equivalent.
+ * This method is like the strict equality operator (`===`) except that it considers `NaN` equal to `NaN`.
+ *
+ * @param {unknown} value The value to compare.
+ * @param {unknown} other The other value to compare.
+ * @returns {boolean} Returns `true` if the values are equivalent, else `false`.
  */
 export function eq(value: unknown, other: unknown): boolean {
     // eslint-disable-next-line no-self-compare
